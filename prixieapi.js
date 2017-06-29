501672e21e0ad935fe8777772ecfa6b48de6ffaf
santhosh <santhu4399@gmail.com>
2017-06-29 12:19:19 +0530

    prixieapi

--- prixieapi.js
+++ prixieapi.js
@@ -72,17 +72,17 @@ app.get('/interview_schedule/:index',function(req, res){
     });
 });
 
-app.get('/walkins/:jobrole',function(req, res){
-    MongoClient.connect(mongosandboxurl,function(err,db){
-          var collection = db.collection("walkins");
-          collection.find({Job_Role:req.params.jobrole},{"_id":0}).toArray(function(err,data){
-              if(err) throw err;
-              console.log(parseInt(req.params.index)+1);
-              db.close();
-              res.send(data[parseInt(req.params.index)+1]);
-          });
-    });
-});
+//app.get('/walkins/:jobrole',function(req, res){
+  //  MongoClient.connect(mongosandboxurl,function(err,db){
+    //      var collection = db.collection("walkins");
+      //    collection.find({Job_Role:req.params.jobrole},{"_id":0}).toArray(function(err,data){
+        //      if(err) throw err;
+          //    console.log(parseInt(req.params.index)+1);
+            //  db.close();
+              //res.send(data[parseInt(req.params.index)+1]);
+//          });
+//    });
+//});
 
 app.get('/walkins/:jobrole/',function(req, res){
     MongoClient.connect(mongosandboxurl,function(err,db){
