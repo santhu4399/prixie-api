1103505c7dc3aaf3b0b0bbdfda7834a193eab850
kanaka gude <yogi87.cse@gmail.com>
2017-07-07 16:21:50 +0530

    yala

--- prixieapi.js
+++ prixieapi.js
@@ -118,8 +118,8 @@ app.get('/get_walkins_by_Walk_In_date/:Walk_In_date/',function(req, res){
           var walkin_date = new Date(req.params.Walk_In_date);
           console.log(walkin_date);
           var collection = db.collection("walkins");
-          collection.find({$and: [{"Walk_In_date.From":{"$gt" : { "$date" :req.params.Walk_In_date }}},
-                                  {"Walk_In_date.To":{"$lt" : { "$date" :req.params.Walk_In_date }}}]},
+          collection.find({$elemMatch: {"Walk_In_date.From":{"$gte" : { "$date" :walkin_date }}},
+                                  {"Walk_In_date.To":{"$lte" : { "$date" :walkin_date }}}},
                               {"_id":0}).toArray(function(err,data){
               if(err) throw err;
               db.close();
